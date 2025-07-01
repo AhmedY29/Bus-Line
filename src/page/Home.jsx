0906dba42ab1b09b42a3036fd7310c7a5e6999ff
Sulaiman-F <s.alfawzan18@gmail.com>
2025-07-01 12:48:37 +0300

    feat: add HomeStudent component with feature cards to enhance user experience for home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LandingPage from "../container/HomePage/LandingPage";
+import HomeStudent from "../container/HomePage/HomeStudent";
 function Home() {
   return (
     <>
       <LandingPage />
+      <HomeStudent />
     </>
   );
 }
