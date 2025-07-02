74f15247e279874b19e05890bf0f3c9d6f22f9c9
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-07-02 09:40:42 +0300

    update sidebar

--- src/container/driverContainerPages/DashboardContainer/DriverSidebar.jsx
+++ src/container/driverContainerPages/DashboardContainer/DriverSidebar.jsx
@@ -21,7 +21,7 @@ function DriverSidebar() {
     { icon: LuBus, label: "Passengers" },
     { icon: IoLocationOutline, label: "Requests" },
     { icon: HiMap, label: "Reports" },
-    { icon: LuBus, label: "AddCar" },
+   
   ];
 
   const adminBottomItems = [
