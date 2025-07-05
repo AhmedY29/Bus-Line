8b43f32ebc81309b277db343bf2d55d50efd65e5
Sulaiman-F <s.alfawzan18@gmail.com>
2025-07-05 20:36:52 +0300

    Update heading text case in HomeReviews component for consistency

--- src/container/HomePage/HomeReviews.jsx
+++ src/container/HomePage/HomeReviews.jsx
@@ -5,7 +5,7 @@ function HomeReviews() {
     <div className="w-full bg-[#f4f7fa] pt-10 pb-15">
       <div className="max-w-7xl mx-auto px-4 md:px-8 ">
         <h1 className="text-lg md:text-2xl lg:text-3xl text-center text-gray-900 font-bold pb-10 ">
-          What our clients say
+          What Our Clients Say
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
