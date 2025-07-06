bd24d7a9ab7155a78cfd12c557513dd61ccd4b42
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-07-06 17:31:02 +0300

    update driver dashboard

--- src/container/driverContainerPages/DashboardContainer/StatCard.jsx
+++ src/container/driverContainerPages/DashboardContainer/StatCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StatCard = ({ title, value, icon }) => {
   return (
     <div className="flex items-center bg-white rounded-lg gap-4 pl-4 h-35 w-full p-4">
-      <div className="flex items-center rounded-lg gap-2 w-full md:w-46 lg:w-50 hover:shadow-lg">
+      <div className="flex items-center rounded-lg gap-2 w-full md:w-46 lg:w-50 ">
         <div className='flex flex-col items-center w-full'>
           <p className="font-bold text-lg">{title}</p>
           <div className="flex items-center gap-3 p-3 rounded-full">
