e75faa865919da4c7970352dfb7fa6802f62bc0f
Sulaiman-F <s.alfawzan18@gmail.com>
2025-07-07 11:22:01 +0300

    Add confirmation toast for logout in SideBar, AppSidebar, and DriverSidebar components

--- src/container/driverContainerPages/DashboardContainer/DriverSidebar.jsx
+++ src/container/driverContainerPages/DashboardContainer/DriverSidebar.jsx
@@ -98,8 +98,7 @@ function DriverSidebar() {
           <li
             className="flex items-center px-3 py-2 rounded-lg text-sm font-medium text-red-600 hover:bg-red-50 hover:text-red-700 gap-2 pr-6 cursor-pointer"
             onClick={() => {
-
-              toast(
+       toast(
                 (t) => (
                   <div className="flex flex-col gap-3">
                     <div className="flex items-center">
