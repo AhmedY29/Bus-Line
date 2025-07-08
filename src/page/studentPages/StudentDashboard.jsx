50abfe244a5f78e9f9490c973f6af00cfe8fe5c7
Thekra <thekra.aljagthmi@com>
2025-07-08 13:49:15 +0300

    Update on Dashboard

--- src/page/studentPages/StudentDashboard.jsx
+++ src/page/studentPages/StudentDashboard.jsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Progress } from "@/components/ui/progress";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import axios from "axios";
 
 // Helper functions to fetch data from the API
